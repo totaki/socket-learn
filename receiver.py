a29ee44133595f9dfc2f9b843050e4fb9b09d721
Sergei Emelianov <karagabul@gmail.com>
2019-02-15 15:02:10 +0300

    Сделал нормальный sender, на самом деле сообщения можно выводить куда-то в одно место. Что-бы было наглядней

--- receiver.py
+++ receiver.py
@@ -12,7 +12,7 @@ async def handle_echo(reader, writer):
     print("Received %r from %r" % (message, addr))
 
     print("Send: %r" % message)
-    writer.write(data)
+    writer.write(b'echo: %s' % data)
     await writer.drain()
 
     print("Close the client socket")
@@ -22,14 +22,13 @@ loop = asyncio.get_event_loop()
 coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
 server = loop.run_until_complete(coro)
 
-# Serve requests until Ctrl+C is pressed
+
 print('Serving on {}'.format(server.sockets[0].getsockname()))
 try:
     loop.run_forever()
 except KeyboardInterrupt:
     pass
-
-# Close the server
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+finally:
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
--- sender.py
+++ sender.py
@@ -1,31 +1,45 @@
 """
 Это скрипт будет подключаться каждый случайный интервал времени от 1 до 3 секунда, с шагом в секунду, писать в socket
-сообщение с каким-то id и выводить ответ через logging
+сообщение с каким-то id и выводить ответ через print
 """
+import time
 import argparse
 import random
 import asyncio
 
+LIST_INT_RUN_FLAG = [True]
+LIST_INT_CURRENT_ID = [1]
 
-async def tcp_echo_client(message, loop):
-    reader, writer = await asyncio.open_connection('127.0.0.1', 8888,
-                                                   loop=loop)
 
-    print('Send: %r' % message)
+async def tcp_echo_client(int_id: int, event_loop: 'asyncio.AbstractEventLoop'):
+    float_start = time.time()
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8888, loop=event_loop)
+    message = '{:0>4}'.format(int_id)
     writer.write(message.encode())
-
     data = await reader.read(100)
-    print('Received: %r' % data.decode())
-
-    print('Close the socket')
+    float_delta = time.time() - float_start
+    print('{:>10.6f} | {:>10}'.format(float_delta, data.decode()))
     writer.close()
 
 
+async def sender_loop(event_loop: 'asyncio.AbstractEventLoop', int_max_delay: int):
+    while LIST_INT_RUN_FLAG[0]:
+        int_id = LIST_INT_CURRENT_ID[0]
+        await asyncio.create_task(tcp_echo_client(int_id, event_loop))
+        LIST_INT_CURRENT_ID[0] += 1
+        await asyncio.sleep(random.randint(1, int_max_delay))
+
+
 def main(int_max_delay: int):
-    message = 'Hello World!'
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(tcp_echo_client(message, loop))
-    loop.close()
+    try:
+        loop.run_until_complete(sender_loop(loop, int_max_delay=int_max_delay))
+    except KeyboardInterrupt:
+        LIST_INT_RUN_FLAG[0] = False
+        tasks = asyncio.all_tasks(loop)
+        loop.run_until_complete(asyncio.gather(*tasks))
+    finally:
+        loop.close()
 
 
 if __name__ == '__main__':
