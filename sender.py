45c3a0489c88aee76dc110b979b799930633d34a
Sergei Emelianov <karagabul@gmail.com>
2019-02-15 09:16:50 +0300

    Сделал описание сервера, добавил копированием примеры tcp сервера и клиента на asyncio, в общем то tornado и не надо тянуть

--- sender.py
+++ sender.py
@@ -1,3 +1,35 @@
 """
-Это скрипт будет подключаться каждую секунду, писать в socket сообщение с каким-то id и выводить ответ через logging
+Это скрипт будет подключаться каждый случайный интервал времени от 1 до 3 секунда, с шагом в секунду, писать в socket
+сообщение с каким-то id и выводить ответ через logging
 """
+import argparse
+import random
+import asyncio
+
+
+async def tcp_echo_client(message, loop):
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8888,
+                                                   loop=loop)
+
+    print('Send: %r' % message)
+    writer.write(message.encode())
+
+    data = await reader.read(100)
+    print('Received: %r' % data.decode())
+
+    print('Close the socket')
+    writer.close()
+
+
+def main(int_max_delay: int):
+    message = 'Hello World!'
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(tcp_echo_client(message, loop))
+    loop.close()
+
+
+if __name__ == '__main__':
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument('max_delay', type=int)
+    namespace = argument_parser.parse_args()
+    main(namespace.max_delay)
